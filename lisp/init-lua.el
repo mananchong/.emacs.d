9a45aa4b3384d2d5f55439ee96ad43d0db4c7c83
mananchong <mananchong@gmail.com>
2022-06-29 23:53:20 +0800

    Create init-lua.el

---
+++ lisp/init-lua.el
@@ -0,0 +1,11 @@
+;;; init-lua.el --- Support for lua-mode -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package lua-mode
+  :staight t
+  :mode "\\.lua\\'")
+
+
+(provide 'init-lua)
+;;; init-lua.el ends here
