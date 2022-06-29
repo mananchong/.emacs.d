f8d52e52ff9ec2ef01d586cda79af13816d0b1d7
mananchong <mananchong@gmail.com>
2022-06-29 23:11:57 +0800

    Update init.el
    try to use straight.el

--- init.el
+++ init.el
@@ -57,6 +57,21 @@
 (if (file-exists-p custom-file)
     (load custom-file))
 
+;; straight.el
+;; https://github.com/radian-software/straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (require 'init-base)
 (require 'init-better-defaults)
 (require 'init-helm)
