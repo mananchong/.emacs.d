ebedb7b3c7784ab2368cadcfbe97785c60bbe26e
mananchong <mananchong@gmail.com>
2023-05-28 18:58:48 +0800

    modify r config

--- lisp/manjis-r.el
+++ lisp/manjis-r.el
@@ -3,7 +3,15 @@
 
 ;;; Code:
 
-(use-package ess)
+(use-package ess
+  :ensure t
+  ;; :bind (:map ess-mode-map
+              ;; ("C-n" . ess-eval-line-and-step))
+  :config
+  (setq comint-scroll-to-bottom-on-input t)
+  (setq comint-scroll-to-bottom-on-output t)
+  (setq comint-move-point-for-output t)
+  :mode ("\\.R\\'" . R-mode))
 
 (provide 'manjis-r)
 ;;; manjis-r ends here
